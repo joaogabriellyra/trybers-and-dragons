6877adc6f908d8b86d36d915f92b7c57cfaaa9ac
João Gabriel Lyra <jgabriellyra@hotmail.com>
2022-11-07 23:07:51 -0300

    requisitos 6 e 7 concluídos

---
+++ src/Character.ts
@@ -0,0 +1,67 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getOneToTen from './utils/getOneToTen';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _energy: Energy;
+  private _dexterity: number;
+  constructor(
+    name: string,
+  ) {
+    this._dexterity = getOneToTen();
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getOneToTen();
+    this._defense = getOneToTen();
+    this._energy = { type_: this._archetype.energyType, amount: getOneToTen() };
+  }
+
+  get race(): Race { return this._race; }
+  get archetype(): Archetype { return this._archetype; }
+  get lifePoints(): number { return this._lifePoints; }
+  get strength(): number { return this._strength; }
+  get defense(): number { return this._defense; }
+  get dexterity(): number { return this._dexterity; }
+  get energy(): Energy { return { ...this._energy }; }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += getOneToTen();
+    this._strength += getOneToTen();
+    this._dexterity += getOneToTen();
+    this._defense += getOneToTen();
+    this._energy.amount = 10;
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._lifePoints = this._maxLifePoints;
+  }
+
+  receiveDamage(attackPointes: number): number {
+    const damage = attackPointes - this._defense;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+      this.checkLifePoints();
+    }
+    return this.lifePoints;
+  }
+
+  checkLifePoints(): void {
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+  }
+}
---
+++ src/utils/getOneToTen.ts
@@ -0,0 +1,4 @@
+export default function getOneToTen() {
+  const randomNumber = Math.random() * (10 - 1) + 1;
+  return randomNumber;
+}
